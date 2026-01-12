d6e7603bb262b9974bb0df8f6b8e57e5a4332384
sAmH365 <npower5377@naver.com>
2026-01-12 13:00:36 +0900

    first commit

---
+++ src/test/java/com/system/splearn/SplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplearnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
