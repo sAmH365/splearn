6ca14ac4f3d9ed3383afc44f4cad8f549cc0050b
sAmH365 <npower5377@naver.com>
2026-01-17 20:40:40 +0900

    add member application port

---
+++ src/main/java/com/system/splearn/application/required/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.system.splearn.application.required;
+
+import com.system.splearn.domain.Member;
+import org.springframework.data.repository.Repository;
+
+/**
+ * 회원 정보를 저장하거나 조회한다
+ */
+public interface MemberRepository extends Repository<Member, Long> {
+  Member save(Member member);
+}
