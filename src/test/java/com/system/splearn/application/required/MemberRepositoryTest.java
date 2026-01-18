c224be6e0c570634ff82a414aab88bc5e3ac74f6
sAmH365 <npower5377@naver.com>
2026-01-18 16:31:14 +0900

    add @NaturalId, @NaturalIdCache

--- src/test/java/com/system/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/com/system/splearn/application/required/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @DataJpaTest
 class MemberRepositoryTest {
@@ -30,5 +31,15 @@ class MemberRepositoryTest {
     assertThat(member.getId()).isNotNull();
 
     entityManager.flush();
+  }
+
+  @Test
+  void duplicateEmailFail() {
+    Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+    memberRepository.save(member);
+
+    Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+    assertThatThrownBy(() -> memberRepository.save(member2)).isInstanceOf(
+        DataIntegrityViolationException.class);
   }
 }
